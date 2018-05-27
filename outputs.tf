fb2387306c4916f14d17134f192cfac408f8f971
Alexander Mancevice <amancevice@cargometrics.com>
2018-05-27 11:16:57 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,11 @@
+output "event_subscriptions_url" {
+  value = "${google_cloudfunctions_function.event_publisher.https_trigger_url}"
+}
 
+output "redirect_url" {
+  value = "${google_cloudfunctions_function.redirect.https_trigger_url}"
+}
+
+output "slash_command_url" {
+  value = "${google_cloudfunctions_function.slash_command.https_trigger_url}"
+}

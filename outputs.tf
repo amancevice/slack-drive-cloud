9ad07e9c37b8448fc31ea0c80216031633209eee
Alexander Mancevice <amancevice@cargometrics.com>
2018-05-27 11:26:08 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "event_subscriptions_url" {
-  value = "${google_cloudfunctions_function.event_publisher.https_trigger_url}"
+  description = "Endpoint for event subscriptions to configure in Slack."
+  value       = "${google_cloudfunctions_function.event_publisher.https_trigger_url}"
 }
 
 output "redirect_url" {
-  value = "${google_cloudfunctions_function.redirect.https_trigger_url}"
+  description = "Endpoint of redirect for slash command links to configure in Slack Drive."
+  value       = "${google_cloudfunctions_function.redirect.https_trigger_url}"
 }
 
 output "slash_command_url" {
-  value = "${google_cloudfunctions_function.slash_command.https_trigger_url}"
+  description = "Endpoint for slash commands to configure in Slack."
+  value       = "${google_cloudfunctions_function.slash_command.https_trigger_url}"
 }

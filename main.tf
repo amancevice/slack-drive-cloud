f3bc76460d4ab74652b6b81c9cf41f38687ee9c0
Alexander Mancevice <alexander.mancevice@gmail.com>
2018-06-02 10:16:25 -0400

    module-ize source code

--- main.tf
+++ main.tf
@@ -1,132 +1,19 @@
-// Config template
-data "template_file" "config" {
-  template = "${file("${path.module}/slack-drive/config.example.json")}"
-
-  vars {
-    project                = "${var.project}"
-    region                 = "${var.region}"
-    events_pubsub_topic    = "${var.events_pubsub_topic}"
-    redirect_function_name = "${var.redirect_function_name}"
-    web_api_token          = "${var.web_api_token}"
-    verification_token     = "${var.verification_token}"
-    channel                = "${var.channel}"
-    color                  = "${var.color}"
-    slash_command          = "${var.slash_command}"
-  }
-}
-
-// Event Consumer archive
-data "archive_file" "event_consumer" {
-  type        = "zip"
-  output_path = "${path.module}/dist/${var.event_consumer_function_name}.zip"
-
-  source {
-    content  = "${file("${path.module}/slack-drive/src/event-consumer/index.js")}"
-    filename = "index.js"
-  }
-
-  source {
-    content  = "${file("${path.module}/slack-drive/src/event-consumer/package.json")}"
-    filename = "package.json"
-  }
-
-  source {
-    content  = "${file("${path.module}/slack-drive/src/messages.json")}"
-    filename = "messages.json"
-  }
-
-  source {
-    content  = "${data.template_file.config.rendered}"
-    filename = "config.json"
-  }
-
-  source {
-    content  = "${file("client_secret.json")}"
-    filename = "client_secret.json"
-  }
-}
-
-// Event Publisher archive
-data "archive_file" "event_publisher" {
-  type        = "zip"
-  output_path = "${path.module}/dist/${var.event_publisher_function_name}.zip"
-
-  source {
-    content  = "${file("${path.module}/slack-drive/src/event-publisher/index.js")}"
-    filename = "index.js"
-  }
-
-  source {
-    content  = "${file("${path.module}/slack-drive/src/event-publisher/package.json")}"
-    filename = "package.json"
-  }
-
-  source {
-    content  = "${data.template_file.config.rendered}"
-    filename = "config.json"
-  }
-
-  source {
-    content  = "${file("client_secret.json")}"
-    filename = "client_secret.json"
-  }
-}
-
-// Redirect archive
-data "archive_file" "redirect" {
-  type        = "zip"
-  output_path = "${path.module}/dist/${redirect_function_name}.zip"
-
-  source {
-    content  = "${file("${path.module}/slack-drive/src/redirect/index.js")}"
-    filename = "index.js"
-  }
-
-  source {
-    content  = "${file("${path.module}/slack-drive/src/redirect/package.json")}"
-    filename = "package.json"
-  }
-
-  source {
-    content  = "${file("${path.module}/slack-drive/src/messages.json")}"
-    filename = "messages.json"
-  }
-
-  source {
-    content  = "${data.template_file.config.rendered}"
-    filename = "config.json"
-  }
-
-  source {
-    content  = "${file("client_secret.json")}"
-    filename = "client_secret.json"
-  }
-}
-
-// Redirect archive
-data "archive_file" "slash_command" {
-  type        = "zip"
-  output_path = "${path.module}/dist/${var.slash_command_function_name}.zip"
-
-  source {
-    content  = "${file("${path.module}/slack-drive/src/slash-command/index.js")}"
-    filename = "index.js"
-  }
-
-  source {
-    content  = "${file("${path.module}/slack-drive/src/slash-command/package.json")}"
-    filename = "package.json"
-  }
-
-  source {
-    content  = "${file("${path.module}/slack-drive/src/messages.json")}"
-    filename = "messages.json"
-  }
-
-  source {
-    content  = "${data.template_file.config.rendered}"
-    filename = "config.json"
-  }
+// Source code module
+module "slack_drive" {
+  source                        = "github.com/amancevice/slack-drive?ref=0.2.0"
+  channel                       = "${var.channel}"
+  project                       = "${var.project}"
+  service_account               = "${var.service_account}"
+  verification_token            = "${var.verification_token}"
+  web_api_token                 = "${var.web_api_token}"
+  color                         = "${var.color}"
+  events_pubsub_topic           = "${var.events_pubsub_topic}"
+  event_consumer_function_name  = "${var.event_consumer_function_name}"
+  event_publisher_function_name = "${var.event_publisher_function_name}"
+  redirect_function_name        = "${var.redirect_function_name}"
+  region                        = "${var.region}"
+  slash_command                 = "${var.slash_command}"
+  slash_command_function_name   = "${var.slash_command_function_name}"
 }
 
 // Cloud Storage Bucket for storing Cloud Function archives
@@ -146,28 +33,28 @@ resource "google_storage_bucket_iam_member" "member" {
 resource "google_storage_bucket_object" "event_consumer_archive" {
   bucket = "${google_storage_bucket.slack_drive_bucket.name}"
   name   = "${var.bucket_prefix}${var.event_consumer_function_name}.zip"
-  source = "${data.archive_file.event_consumer.output_path}"
+  source = "${module.slack_drive.event_consumer_output_path}"
 }
 
 // Event Publisher Cloud Storage archive
 resource "google_storage_bucket_object" "event_publisher_archive" {
   bucket = "${google_storage_bucket.slack_drive_bucket.name}"
   name   = "${var.bucket_prefix}${var.event_publisher_function_name}.zip"
-  source = "${data.archive_file.event_publisher.output_path}"
+  source = "${module.slack_drive.event_publisher_output_path}"
 }
 
 // Redirect Cloud Storage archive
 resource "google_storage_bucket_object" "redirect_archive" {
   bucket = "${google_storage_bucket.slack_drive_bucket.name}"
   name   = "${var.bucket_prefix}${var.redirect_function_name}.zip"
-  source = "${data.archive_file.redirect.output_path}"
+  source = "${module.slack_drive.redirect_output_path}"
 }
 
 // Slash Command Cloud Storage archive
 resource "google_storage_bucket_object" "slash_command_archive" {
   bucket = "${google_storage_bucket.slack_drive_bucket.name}"
   name   = "${var.bucket_prefix}${var.slash_command_function_name}.zip"
-  source = "${data.archive_file.slash_command.output_path}"
+  source = "${module.slack_drive.slash_command_output_path}"
 }
 
 // Pub/Sub Topic for processing events

c3ea17e965955911d458cc09ea5cba463dfe0fd3
Alexander Mancevice <amancevice@cargometrics.com>
2018-06-02 11:46:57 -0400

    slack-drive ~> 0.3.1

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 // Source code module
 module "slack_drive" {
-  source                        = "github.com/amancevice/slack-drive?ref=0.3.0"
+  source                        = "github.com/amancevice/slack-drive?ref=0.3.1"
   channel                       = "${var.channel}"
   project                       = "${var.project}"
   service_account               = "${var.service_account}"
